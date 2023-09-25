700c240431f566107e5edcbea366045708cda2d4
lourdsam <17100411@usc.edu.ph>
2023-09-26 00:24:37 +0800

    added about us page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import MerchantLayout from './components/layout/MerchantLayout';
 import AdminLayout from './components/layout/AdminLayout';
 
 import ChoicePage from './pages/user/eateryChoices/choicePage.tsx';
+import AboutUsPage from './pages/user/about/aboutUsPage.tsx';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route element={<UserLayout />}>
             <Route path="/eaterychoice" element={<ChoicePage />} />
+            <Route path="/aboutus" element={<AboutUsPage />} />
             {/* Insert your page here */}
           </Route>
           <Route element={<MerchantLayout />}>
---
+++ src/pages/user/about/aboutUsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import colors from '../../../common/colors'
+
+const AboutUsPage = () => {
+  return (
+    <div className={`bg-[#F9F2EA] h-[80vh]`}>
+        About us page
+    </div>
+  )
+}
+
+export default AboutUsPage
