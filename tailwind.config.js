739fbda74ffe0ba78fef0a94fc30368acf9cdd9e
Katteu <katheamari@gmail.com>
2023-09-25 22:04:02 +0800

    [update] user layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins', 'sans'],
+        'poppins': ['Poppins','sans-serif'],
       },
       keyframes: {
         'slide-down': {
