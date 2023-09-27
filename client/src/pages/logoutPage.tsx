7bef7fd65e99521a871856a5d62f36132f5e3a90
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-27 22:43:48 +0800

    adding background image

--- client/src/pages/logoutPage.tsx
+++ client/src/pages/logoutPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import BG from '../assets/background-pattern.png'
 
 const LogoutPage = () => {
   return (
-    <div>L</div>
+    <div className="bg-[url('../assets/background-pattern.png')]">
+
+    </div>
   )
 }
 
