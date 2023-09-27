b69d3f4a917f3bf21885ca87893531c00bb20c64
Clyde <noobclyde21@gmail.com>
2023-09-28 00:42:02 +0800

    Finished UI

---
+++ src/pages/merchant/settings/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Header(){
+    return(
+        <div className="flex-1 w-100 bg-white h-16 px-4">
+            <h1>
+                Hello world
+            </h1>
+        </div>
+    )
+}
