24f90f0e7aefc472c7376f41fbc674707212b77d
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-27 15:01:13 +0800

    70% done

--- src/pages/admin/accountlist/accountList.tsx
+++ src/pages/admin/accountlist/accountList.tsx
@@ -2,38 +2,72 @@ import React from 'react'
 
 const AccountList = () => {
   return (
-    <div className='font-poppins shadow-2xl mx-5 my-5 p-5 rounded-lg'>
+    <div className='font-poppins shadow-2xl mx-5 my-5 p-5 rounded-lg w-fit h-screen'>
 
       <label htmlFor="filterList" className='text-slate-600'>Filter by: </label>
-      <select name="filterList" id="filtersList" className='rounded-lg border border-black m-4 mr-40'>
+      <select name="filterList" id="filtersList" className='rounded-lg border border-black m-4 mr-80 py-1 px-5'>
         <option value="allAccounts">All Accounts</option>
         <option value="none">None</option>
       </select>
 
-      <label htmlFor="searchBar">Search: </label>
-      <input type="text" placeholder='Input name or email' id='searchBar' className='rounded-lg border border-slate-500'/>
+      <label htmlFor="searchBar" className='ml-80'>Search: </label>
+      <input type="text" placeholder='Input name or email' id='searchBar' className='rounded-lg border border-slate-500 px-3 py-1'/>
       
       <div className='relative flex flex-col'>
-        <table className='table-fixed w-auto'>
+        <table className='table-auto'>
           <thead className='border-black border-y'>
             <tr>
-              <th className='text-md px-4 py-3'>ID</th>
-              <th className='text-md px-4 py-3'>Account Name</th>
-              <th className='text-md px-4 py-3'>E-mail Address</th>
-              <th className='text-md px-4 py-3'>Contact Number</th>
-              <th className='text-md px-4 py-3'>Type</th>
-              <th className='text-md px-4 py-3'>Status</th>
-              <th className='text-md px-4 py-3'>Manage</th>
+              <th className='px-5'>Account Name</th>
+              <th className='px-5'>ID</th>
+              <th className='px-5'>E-mail Address</th>
+              <th className='px-5'>Contact Number</th>
+              <th className='px-5'>Type</th>
+              <th className='px-5'>Status</th>
+              <th className='px-5'>Manage</th>
             </tr>
           </thead>
           <tbody className='text-center'>
             <tr>
-              <td className='text-md px-5 py-3'>1</td>
-              <td className='text-md px-5 py-3'>Franz Casimir Ondiano</td>
-              <td className='text-md px-5 py-3'>franzcasimir.ondiano@gmail.com</td>
-              <td className='text-md px-5 py-3'>09478517200</td>
-              <td className='text-md px-5 py-3'>Customer</td>
+              <td className='text-md px-10 py-3'>1</td>
+              <td className='text-md px-12 py-3'>Franz Casimir Ondiano</td>
+              <td className='text-md px-12 py-3'>franzcasimir.ondiano@gmail.com</td>
+              <td className='text-md px-12 py-3'>09478517200</td>
+              <td className='text-md px-12 py-3'>Customer</td>
+              <td className='text-md px-12 py-3'>
+                <button className='bg-emerald-200 rounded-lg px-4 text-green-900'>
+                  Active
+                </button>
+              </td>
               <td className='text-md px-5 py-3'>
+                <button className='bg-red-600 text-white rounded-lg px-4'>
+                  Delete
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className='text-md px-10 py-3'>2</td>
+              <td className='text-md px-12 py-3'>Matthew Dave Desabelle</td>
+              <td className='text-md px-12 py-3'>desabellematthew@gmail.com</td>
+              <td className='text-md px-12 py-3'>09123456789</td>
+              <td className='text-md px-12 py-3'>Customer</td>
+              <td className='text-md px-12 py-3'>
+                <button className='bg-emerald-200 rounded-lg px-4 text-green-900'>
+                  Active
+                </button>
+              </td>
+              <td className='text-md px-5 py-3'>
+                <button className='bg-red-600 text-white rounded-lg px-4'>
+                  Delete
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className='text-md px-10 py-3'>3</td>
+              <td className='text-md px-12 py-3'>Lourd Sam Abrigos</td>
+              <td className='text-md px-12 py-3'>bobot@gmail.com</td>
+              <td className='text-md px-12 py-3'>0955555555</td>
+              <td className='text-md px-12 py-3'>Customer</td>
+              <td className='text-md px-12 py-3'>
                 <button className='bg-emerald-200 rounded-lg px-4 text-green-900'>
                   Active
                 </button>
@@ -46,6 +80,9 @@ const AccountList = () => {
             </tr>
           </tbody>
         </table>
+        <div className='text-slate-500 font-semibold text-right'>
+          <p>Accounts as of September 27, 2023</p>
+        </div>
       </div>
     </div>
   )
