67bc621d622d37996a340dd39797beee2e74b3b6
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-27 22:03:40 +0800

    test

--- client/src/pages/logoutPage.tsx
+++ client/src/pages/logoutPage.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const LogoutPage = () => {
   return (
-    <div className='bg-[url(../assets/background-pattern.png)]'>
-
-    </div>
+    // <div className='bg-[url(../assets/background-pattern.png)]'>
+    <div></div>
+    // </div>
   )
 }
 
