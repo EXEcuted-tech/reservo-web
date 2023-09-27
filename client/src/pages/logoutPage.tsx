a1cb2c8666efd22b8a5088508bc02ffd352e3a0e
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-28 02:31:17 +0800

    added wavy background in logout page

--- client/src/pages/logoutPage.tsx
+++ client/src/pages/logoutPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
-// import BG from '../assets/background-pattern.png'
+import Background from '../assets/background-pattern.png'
+import {RiReservedFill} from 'react-icons/ri'
 
 const LogoutPage = () => {
   return (
-    <div>
-      FUCK
+    <div className='relative'>
+      <div className='h-[10vh] w-full border-b-8 border-red-600'>
+        <RiReservedFill className='fill-red-600 text-[80px] my-[10px] ml-[50px]' />
+      </div>
+      <img src={Background} alt='wavy-pattern' className='object-cover h-[88vh] w-full absolute mix-blend-overlay bg-no-repeat' />
+      FUCKING BULLSHIT
     </div>
   )
 }
