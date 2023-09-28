b5125a6255239208f1f89851097382ddc343c269
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-28 17:40:01 +0800

    finished UI for logout page

--- client/src/pages/logoutPage.tsx
+++ client/src/pages/logoutPage.tsx
@@ -26,8 +26,10 @@ const LogoutPage = () => {
       <div className='flex flex-row justify-even place-items-center bg-logout'>
         <div className='flex flex-col'>
             <h1 className='font-extrabold text-9xl ml-20 text-red-700'>You have been logged out.</h1>
-            <span className='font-semibold text-4xl ml-20 mt-[50px]'>Thank you for using Reservo.<br/>To access the page, please log in again.</span>
-            <Link to={'/uslogin'} className='bg-red-700 rounded-full text-center text-white text-4xl font-semibold py-3 px-6 mt-7 ml-20'>Return to Login Page</Link>
+            <span className='font-semibold text-4xl ml-20 mt-[50px]'>Thank you for using Reservo.<br/>To access the page, please log in again.</span>     
+          <div className='flex'>
+            <Link to={'/uslogin'} className='bg-red-700 rounded-full text-center text-white text-4xl font-semibold py-3 px-6 mt-7 ml-20 m-[1rem]'>Return to Login Page</Link>
+          </div>
         </div>
 
         <div>
