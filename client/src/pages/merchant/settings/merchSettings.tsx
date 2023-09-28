a18e52eeee31dda632bde9cd6dedcaac9df91885
henneh <galvehannah5302@gmail.com>
2023-09-28 20:41:21 +0800

    [FIX] UI Fix for Merchant Settings

--- client/src/pages/merchant/settings/merchSettings.tsx
+++ client/src/pages/merchant/settings/merchSettings.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
-
+import { BsFillBuildingFill } from "react-icons/bs";
+import MerchAdHeader from '../../../components/headers/MerchAdHeader.tsx';
+import { Divider } from '@mui/material';
 import GeneralSettings from './generalSettings';
 import AdvancedSettings from './advancedSettings';
 
@@ -11,7 +13,10 @@ const MerchSettings = () => {
     };
 
     return (
-        <>
+        <div className={`bg-[#FFFFFF] h-[100vh] font-poppins overflow-y-auto overflow-x-hidden`}>
+        <div className="w-[80vw]">
+        <MerchAdHeader icon={BsFillBuildingFill} title={'Merchant Settings'}/>
+
             <div className="bg-[#F3F3F3] h-full w-full">
  
                 <div style={{fontFamily: 'Poppins, sans-serif'}} className="flex ml-10 mr-10">
@@ -21,6 +26,7 @@ const MerchSettings = () => {
                     >
                         General Settings
                     </div>
+                   
                     <div
                         className={`hover:underline tab-item cursor-pointer p-4 transition duration-300 ease-in-out ${activeTab === 0 ? 'font-bold' : ''}`}
                         onClick={() => handleTabClick(2)}
@@ -31,11 +37,13 @@ const MerchSettings = () => {
                         className={`underline transition-transform duration-300 ease-in-out ${activeTab === 0 ? 'translate-x-0' : activeTab === 1 ? 'translate-x-full' : 'translate-x-full*2'}`}
                     ></div>
                 </div>
+
+                <Divider />
                 {activeTab === 1 && <GeneralSettings />}
                 {activeTab === 2 && <AdvancedSettings />}
             </div>
-        </>
-       
+            </div>
+        </div>  
     )
 }
 
