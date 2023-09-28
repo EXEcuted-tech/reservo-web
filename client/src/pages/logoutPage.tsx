f53d5a2046b4ec6e6091cbbd1503636787a8bdbd
Matthew Desabelle <desabellematthew@gmail.com>
2023-09-28 17:02:35 +0800

    80% done

--- client/src/pages/logoutPage.tsx
+++ client/src/pages/logoutPage.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import Background from '../assets/background-pattern.png'
 import {RiReservedFill} from 'react-icons/ri'
+import PeopleWaving from '../assets/wave.png'
+
+// module.exports = {
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         'bg-image': "url('../assets/background-pattern.png')",
+//       }
+//     }
+//   }
+// };
 
 const LogoutPage = () => {
   return (
@@ -8,11 +19,18 @@ const LogoutPage = () => {
       <div className='h-[10vh] w-full border-b-8 border-red-600'>
         <RiReservedFill className='fill-red-600 text-[80px] my-[10px] ml-[50px]' />
       </div>
-      <img src={Background} alt='wavy-pattern' className='object-cover h-[88vh] w-full absolute mix-blend-overlay bg-no-repeat' />
+      {/* <img src={Background} alt='wavy-pattern' className='object-cover h-[88vh] w-full absolute mix-blend-overlay bg-no-repeat' /> */}
       
-      <div className='flex justify-around'>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro iste nemo aperiam fugiat ullam tenetur accusamus quis doloremque eveniet atque quod animi vero expedita voluptatum eius, rem provident corrupti harum.</p>
-        <p>dasdas</p>
+      <div className='flex flex-row justify-even pt-20 place-items-center'>
+        <div>
+          <h1 className='font-extrabold text-9xl ml-10 text-red-700'>You have been logged out.</h1>
+          <p className='font-semibold text-4xl ml-10 mt-[50px]'>Thank you for using Reservo.<br/> To access the page, please log in again.</p>
+          <a href='/uslogin'><button>Return to Login Page</button></a>
+        </div>
+
+        <div>
+          <img src={PeopleWaving} alt="people-waving" className='w-[900px] mr-[500px]'/>
+        </div>
       </div>
 
       {/* <div>
