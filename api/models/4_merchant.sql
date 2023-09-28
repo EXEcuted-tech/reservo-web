d1b7ae105ab3dc9627024aa6c1abb6d6bf7f351f
Angelou Sereño <angeloubendebelsereno@gmail.com>
2023-09-29 04:02:02 +0800

    update model

--- api/models/4_merchant.sql
+++ api/models/4_merchant.sql
@@ -1,10 +1,9 @@
 USE reservo;
-/*Merchant needs the following (in order):
- > merchant_sched.sql
-*/
+
+/* Merchant needs the following (in order): > merchant_sched.sql */
 
 CREATE TABLE `merchant` (
-  `merchant_id` bigint(20) NOT NULL,
+  `merchant_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `merchant_name` varchar(30) NOT NULL,
   `email_address` varchar(60) NOT NULL,
   `logo` varchar(255) DEFAULT NULL,
@@ -12,13 +11,7 @@ CREATE TABLE `merchant` (
   `address` varchar(255) DEFAULT NULL,
   `settings` varchar(255) DEFAULT NULL,
   `sched_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`merchant_id`),
+  KEY `sched_id` (`sched_id`),
+  CONSTRAINT `merchant_ibfk_1` FOREIGN KEY (`sched_id`) REFERENCES `merchant_sched` (`sched_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-ALTER TABLE `merchant`
-  ADD PRIMARY KEY (`merchant_id`),
-  ADD KEY `sched_id` (`sched_id`),
-  MODIFY COLUMN `merchant_id` BIGINT AUTO_INCREMENT;
-
-
-ALTER TABLE `merchant`
-  ADD CONSTRAINT `merchant_ibfk_1` FOREIGN KEY (`sched_id`) REFERENCES `merchant_sched` (`sched_id`);
