19658636e3569cb8407bf495233274d93650b8fa
Angelou Sereño <angeloubendebelsereno@gmail.com>
2023-09-29 05:26:33 +0800

    added signup and login api

--- api/controllers/authenticationController.js
+++ api/controllers/authenticationController.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const db = require('../a_db'); 
+const db = require('./a_db'); 
+const saltRounds = 10;
+const bcrypt = require('bcrypt');
 
 const createAccount = async (req, res) => {
     try {
         const { account_name, account_email, password, account_type, contact_number } = req.body;
 
         // Use a parameterized query to prevent SQL injection
-        const sql = "INSERT INTO account (account_name, account_email, password, account_type, contact_number) VALUES (?, ?, ?, ?, ?)";
-        const values = [account_name, account_email, password, account_type, contact_number];
+        const sql = "INSERT INTO account (account_name, email_address, passwd, account_type, contact_number) VALUES (?, ?, ?, ?, ?)";
+        const hashedpassword = await bcrypt.hash(password, saltRounds)
+        const values = [account_name, account_email, hashedpassword, account_type, contact_number];
         db.query(sql, values, (err, result) => {
             if (err) {
                 res.json({
@@ -20,6 +23,8 @@ const createAccount = async (req, res) => {
                     message: "Account created successfully",
                 });
             }
+
+            db.end();
         });
     } catch (error) {
         res.status(500).json({
@@ -30,10 +35,42 @@ const createAccount = async (req, res) => {
 }
 
 const login = (req,res)=>{
-    res.json({
-        success:true,
-        data:[{id:1,text:'Testing Purposes Only'}]
-    })
+    try {
+        const { account_email, password} = req.body;
+
+        // Use a parameterized query to prevent SQL injection
+        const sql = "SELECT email_address, passwd FROM account WHERE email_address = ?";
+        const values = [account_email];
+        db.query(sql, values, (err, result) => {
+            if (!err && result.length === 1){
+                const hash = result[0].passwd;
+            bcrypt.compare(password,hash).then(function(result) {
+                if (result == true){
+                    res.json({
+                        success: true,
+                        message: "Retrieved",
+                    });
+                }else{
+                    res.json({
+                        success: false,
+                        message: "Incorrect Credentials",
+                    });
+                }
+            });
+                
+                db.end();
+        }else{
+            console.log(err);
+        }
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Database Error",
+        });
+    }
+    
+    
 }
 
 module.exports = {
