ccb857ca9b24b6c0a873a443db2ad21a2a722fbf
PonPonc <gybaez321@gmail.com>
2023-09-28 20:16:17 +0800

    Initial Calendar Design

--- client/src/pages/merchant/reservationList/mainPage.tsx
+++ client/src/pages/merchant/reservationList/mainPage.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import MerchAdHeader from '../../../components/headers/MerchAdHeader'
+import Calendar from '../../../components/calendar/Calendar'
 import {BiSolidBookAlt} from 'react-icons/bi'
 
 const ReserveList = () => {
   return (
     <div>
       <MerchAdHeader icon={BiSolidBookAlt} title={'All Bookings'}/>
+      <div className='h-[10vh] bg-green-300'>
+        Yawa Period
+      </div>
+      <Calendar/>
     </div>
   )
 }
