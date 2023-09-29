0549e6dec1f07f6a9bd3139fa0ff3d0321c18fa3
PonPonc <gybaez321@gmail.com>
2023-09-29 10:02:12 +0800

    Slight Fixes

--- api/controllers/authenticationController.js
+++ api/controllers/authenticationController.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const db = require('../a_db'); 
+const db = require('./a_db'); 
 
 const createAccount = async (req, res) => {
     try {
         const { account_name, account_email, password, account_type, contact_number } = req.body;
 
         // Use a parameterized query to prevent SQL injection
-        const sql = "INSERT INTO account (account_name, account_email, password, account_type, contact_number) VALUES (?, ?, ?, ?, ?)";
+        const sql = "INSERT INTO account (account_name, account_email, passwd, account_type, contact_number) VALUES (?, ?, ?, ?, ?)";
         const values = [account_name, account_email, password, account_type, contact_number];
         db.query(sql, values, (err, result) => {
             if (err) {
