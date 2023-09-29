2e87ae8980c7c2b04e07b226da59cb8a5f84056b
Angelou Sereño <angeloubendebelsereno@gmail.com>
2023-09-29 19:59:03 +0800

    deploy

--- api/controllers/a_db.js
+++ api/controllers/a_db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
 });
--- api/index.js
+++ api/index.js
@@ -4,12 +4,12 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 
-const db = mysql.createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-});
+// const db = mysql.createPool({
+//     host:process.env.DB_HOST,
+//     user:process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+// });
   
 
 const PORT = process.env.PORT || 5000; 
