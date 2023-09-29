b55a3667a88db1cda4f4f8917c7972f47719d6ca
Angelou Sereño <angeloubendebelsereno@gmail.com>
2023-09-29 23:06:17 +0800

    update added validations

--- api/validations/packageValidator.js
+++ api/validations/packageValidator.js
@@ -1,8 +1,36 @@
 const createPackageValidator = (req,res,next)=>{
-    if(!req.body.text){
+    if(!req.body.package_name){
         return res.json({
             success:false,
-            error:{text:['text is required']},
+            error:{text:['package name is empty']},
+        })
+    }
+
+    if(!req.body.package_desc){
+        return res.json({
+            success:false,
+            error:{text:['package description is empty']},
+        })
+    }
+
+    if(!req.body.date_start){
+        return res.json({
+            success:false,
+            error:{text:['start date is empty!']},
+        })
+    }
+
+    if(!req.body.date_end){
+        return res.json({
+            success:false,
+            error:{text:['end date is empty!']},
+        })
+    }
+
+    if(!req.body.date_start){
+        return res.json({
+            success:false,
+            error:{text:['start date is empty!']},
         })
     }
 }
