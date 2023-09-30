11f39a68368ec91b23225c8d731d1e033f3fd96e
Angelou Sereño <angeloubendebelsereno@gmail.com>
2023-10-01 00:16:21 +0800

    update CORS

--- api/routes.js
+++ api/routes.js
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
