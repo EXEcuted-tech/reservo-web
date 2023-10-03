49581efa1276fd2a8025017b9c86407279b20edc
Angelou Sereño <angeloubendebelsereno@gmail.com>
2023-10-03 10:59:52 +0800

    change from 30 to 10 for merchant acc

--- api/validations/authenticationValidator.js
+++ api/validations/authenticationValidator.js
@@ -42,7 +42,7 @@ const authenticationValidator = (req,res,next)=>{
     switch(req.body.account_type){
         case 1:
         case 50:
-        case 30:
+        case 10:
             break;
         default:
             return res.status(400).json({
