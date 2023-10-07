f2bdade2931de2971ef2f7580ef7aa1d49b62e05
Matthew Desabelle <desabellematthew@gmail.com>
2023-10-07 16:03:14 +0800

    added pagination

--- client/src/pages/admin/accountlist/accountList.tsx
+++ client/src/pages/admin/accountlist/accountList.tsx
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import MerchAdHeader from '../../../components/headers/MerchAdHeader'
 import { RiUserFill } from 'react-icons/ri';
+import Pagination from '@mui/material/Pagination';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
 import config from '../../../common/config';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#DD2803',
+    },
+  },
+});
 
 interface AccountDetailsProps {
   account_id: number;
@@ -15,24 +25,29 @@ interface AccountDetailsProps {
 }
 
 const AccountList = () => {
-  const [date, setDate] = useState<Date | null>(null);
   const [users, setData] = useState<AccountDetailsProps[]> ([]);
+  const [date, setDate] = useState<Date | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  // Pagination
+  const itemsPerPage = 10;
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentAccountListPage = users.slice(indexOfFirstItem, indexOfLastItem);
+  const handlePageChange = (event: React.ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page);
+  };
 
   useEffect(() => {
     setDate(new Date());
     axios.get(`${config.API}/user/retrieve_all`)
     .then(res => {
       setData(res.data.user);
-      // console.log(res);
     })
     .catch(err => {
       console.log(err);
     })
   }, []);
-
-// ACCOUNT TYPE: 
-// user = 1
-// merchant = 10
 
   return (
     <div className='w-[100%]'>
@@ -45,14 +60,14 @@ const AccountList = () => {
         <option value="stat_active">Status: Active</option>
         <option value="stat_abolished">Status: Abolished</option>
         <option value="type_cust">Type: Customer</option>
-        <option value="type_admin">Type: Admin</option>
+        <option value="type_admin">Type: Merchant</option>
       </select>
 
       <label htmlFor="searchBar" className='ml-[580px]'>Search: </label>
       <input type="text" placeholder='Input name or email' id='searchBar' className='rounded-lg border border-slate-500 px-3 py-1'/>
 
       <div className='flex flex-col'> {/* CONSIDER REMOVING CLASSNAME*/}
-        <div className='flex flex-col h-[70vh]'>
+        <div className='flex flex-col h-[65vh]'>
           <table className='table-auto'>
             <thead className='border-black border-y'>
               <tr>
@@ -66,85 +81,55 @@ const AccountList = () => {
               </tr>
             </thead>
             <tbody className='text-center'>
-              {users.map((account, index) => (
-                <tr>
-                  <td className='text-md px-12 py-3'>{account.account_id}</td>
-                  <td className='text-md px-12 py-3'>{account.account_name}</td>
-                  <td className='text-md px-12 py-3'>{account.email_address}</td>
-                  <td className='text-md px-12 py-3'>{account.contact_number}</td>
-                  <td className='text-md px-12 py-3'>
-                    {account.account_type === 1? 'Customer': 'Merchant'}
-                  </td>
-                  <td className='text-md px-12 py-3'>
-                    <button 
-                      className={'rounded-full px-4 cursor-default ' + (account.account_status === 'active'? 'bg-emerald-200 text-green-900' : 'bg-red-500 text-white')}>
-                      {account.account_status === 'active'? 'Active' : 'Abolished'}
+              {currentAccountListPage.length > 0 ? (
+                currentAccountListPage.map((account, index) => (
+                  <tr>
+                    <td className='text-md px-12 py-3'>{account.account_id}</td>
+                    <td className='text-md px-12 py-3'>{account.account_name}</td>
+                    <td className='text-md px-12 py-3'>{account.email_address}</td>
+                    <td className='text-md px-12 py-3'>{account.contact_number}</td>
+                    <td className='text-md px-12 py-3'>
+                      {account.account_type === 1? 'Customer': 'Merchant'}
+                    </td>
+                    <td className='text-md px-12 py-3'>
+                      <button 
+                        className={'rounded-full px-4 cursor-default ' + (account.account_status === 'active'? 'bg-emerald-200 text-green-900' : 'bg-red-500 text-white')}>
+                        {account.account_status === 'active'? 'Active' : 'Abolished'}
+                      </button>
+                    </td>
+                    <td className='text-md px-5 py-3'>
+                    <button className='bg-red-600 text-white rounded-full px-4'>
+                      Delete
                     </button>
                   </td>
-                  <td className='text-md px-5 py-3'>
-                  <button className='bg-red-600 text-white rounded-full px-4'>
-                    Delete
-                  </button>
-                </td>
-                </tr>
-              ))}
-
-              {/* <tr>
-                <td className='text-md px-12 py-3'>1</td>
-                <td className='text-md px-12 py-3'>Franz Casimir Ondiano</td>
-                <td className='text-md px-12 py-3'>franzcasimir.ondiano@gmail.com</td>
-                <td className='text-md px-12 py-3'>09478517200</td>
-                <td className='text-md px-12 py-3'>Customer</td>
-                <td className='text-md px-12 py-3'>
-                  <button className='bg-emerald-200 rounded-lg px-4 text-green-900 cursor-default'>
-                    Active
-                  </button>
-                </td>
-                <td className='text-md px-5 py-3'>
-                  <button className='bg-red-600 text-white rounded-lg px-4'>
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className='text-md px-10 py-3'>2</td>
-                <td className='text-md px-12 py-3'>Matthew Dave Desabelle</td>
-                <td className='text-md px-12 py-3'>desabellematthew@gmail.com</td>
-                <td className='text-md px-12 py-3'>09123456789</td>
-                <td className='text-md px-12 py-3'>Customer</td>
-                <td className='text-md px-12 py-3'>
-                  <button className='bg-emerald-200 rounded-lg px-4 text-green-900 cursor-default'>
-                    Active
-                  </button>
-                </td>
-                <td className='text-md px-5 py-3'>
-                  <button className='bg-red-600 text-white rounded-lg px-4'>
-                    Delete
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className='text-md px-10 py-3'>3</td>
-                <td className='text-md px-12 py-3'>Lourd Sam Abrigos</td>
-                <td className='text-md px-12 py-3'>bobot@gmail.com</td>
-                <td className='text-md px-12 py-3'>0955555555</td>
-                <td className='text-md px-12 py-3'>Customer</td>
-                <td className='text-md px-12 py-3'>
-                  <button className='bg-emerald-200 rounded-lg px-4 text-green-900 cursor-default'>
-                    Active
-                  </button>
-                </td>
-                <td className='text-md px-5 py-3'>
-                  <button className='bg-red-600 text-white rounded-lg px-4'>
-                    Delete
-                  </button>
-                </td>
-              </tr> */}
+                  </tr>
+                ))
+              ) : (
+                <p>No Accounts</p>
+              )}
             </tbody>
           </table>
         </div>
         <div className='text-slate-500 font-semibold bottom-[50px] right-[80px] text-xl'>
           <h1 className='align-text-bottom text-right'>{`Accounts as of ${date}`}</h1>
+        </div>
+
+        <div className="flex justify-center mb-20 w-[100%] h-[7%]">
+            <ThemeProvider theme={theme}>
+              <Pagination
+                count={Math.ceil(users.length / itemsPerPage)}
+                shape="rounded"
+                showFirstButton
+                showLastButton
+                color="primary"
+                className="absolute bottom-8"
+                onChange={handlePageChange}
+                page={currentPage}
+              />
+            </ThemeProvider>
+            <div className="text-[#969696] text-xs absolute bottom-3">
+              Page {currentPage} of {Math.ceil(users.length / itemsPerPage)}
+            </div>
         </div>
       </div>
       </div>
