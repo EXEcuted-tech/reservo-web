8cb93409cf275b34dc259453082ade6cf4a1b32e
Jeremiah Juinio <8otsoeight@gmail.com>
2023-10-08 22:51:47 +0800

    finished the modal, though no code yet for the database query

--- client/src/pages/user/accountsProfile/modals/editUsername.tsx
+++ client/src/pages/user/accountsProfile/modals/editUsername.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { BsFillPencilFill } from "react-icons/bs";
+import { HiCheck } from "react-icons/hi";
+import { MdCancel } from "react-icons/md";
 
 
 export default function EditUsername(props: { userName : string }){
@@ -16,24 +18,25 @@ export default function EditUsername(props: { userName : string }){
     }
 
     const handleSaveClick = () => {
-
+        // to write code for updating the database (i honestly dont wanna touch the controller, halppp)
+        setIsEdit(false);
     }
 
     return (
         <>
-        
+            
             {isEdit ? (
-                <div className="w-[5cm] h-[5cm] bg-white flex flex-row border border-black">
-                    <input
+                <div className="rounded-[10px] bg-[#EEEEEE] flex flex-row  row items-center w-[10cm] mx-[3vw]">
+                    <input className="w-[7.8cm] ml-[0.3cm] bg-[#EEEEEE]"
                     type="text"
                     value={userName}
                     onChange={(e) => setUserName(e.target.value)}
                     />
-                    <button onClick={handleSaveClick}>Save</button>
-                    <button onClick={handleCancelClick}>Cancel</button>
+                    <button className="h-[15pt] text-[25pt] px-[0.1cm] row flex justify-center items-center text-[#383838]" onClick={handleSaveClick}><HiCheck/></button>
+                    <button className="h-[15pt] text-[18pt] row flex justify-center items-center text-[#383838]" onClick={handleCancelClick}><MdCancel/></button>
                 </div>
             ) : (
-                <button onClick={handleEditClick}><BsFillPencilFill className="ml-1"/></button>
+                <h1>{userName}<button onClick={handleEditClick}><BsFillPencilFill className="ml-1"/></button></h1>
             )}
             
         </>
