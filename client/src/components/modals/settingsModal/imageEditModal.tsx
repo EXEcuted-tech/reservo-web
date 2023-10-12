759d7c9e89c7ef927dccd9a0666cb8cb3d248a77
henneh <galvehannah5302@gmail.com>
2023-10-12 14:22:42 +0800

    [UPDATE] Fixes to Merch Settings Logo Modal

--- client/src/components/modals/settingsModal/imageEditModal.tsx
+++ client/src/components/modals/settingsModal/imageEditModal.tsx
@@ -15,7 +15,7 @@ const ImageEditModal: React.FC<ImageEditModalProps> = ({ isOpen, onClose, onSave
   };
 
   return (
-    <div className={`modal ${isOpen ? 'block animate-fade-in h-[9rem]' : 'hidden'}`}>
+    <div className={`modal ${isOpen ? 'block animate-fade-in h-[9rem] w-[40%] pr-[2%]' : 'hidden'}`}>
       <div className="modal-overlay" onClick={onClose}></div>
 
       <div className="modal-container">
@@ -26,12 +26,12 @@ const ImageEditModal: React.FC<ImageEditModalProps> = ({ isOpen, onClose, onSave
             type="text"
             value={newImageUrl}
             onChange={(e) => setNewImageUrl(e.target.value)}
-            className="border border-gray-300 rounded-md p-2 w-full focus:outline-none focus:ring focus:ring-blue-500"
+            className="border border-gray-300 rounded-md p-2 mb-[0.6rem] w-full focus:outline-none focus:ring focus:ring-blue-500"
             required
           />
         </div>
 
-        <div className="modal-footer p-3 ml-1 ">
+        <div className="modal-footer p-3 ml-[57%] ">
           <button 
                 type="submit"
                 className="btn btn-primary w-[5rem] text-white bg-[#1B6E1E] p-[0.30rem] text-[1.1rem] items-center rounded-lg hover:bg-[#00962a] transition-colors delay-250 duration-[3000] ease-in" 
@@ -39,7 +39,7 @@ const ImageEditModal: React.FC<ImageEditModalProps> = ({ isOpen, onClose, onSave
             Save
           </button>
           <button 
-                className="btn btn-secondary w-[6rem] ml-6 text-white bg-[#840705] p-[0.30rem] text-[1.1rem] items-center rounded-lg hover:bg-[#5a1513] transition-colors delay-250 duration-[3000] ease-in" 
+                className="btn btn-secondary w-[6rem] ml-[0.4rem] text-white bg-[#840705] p-[0.30rem] text-[1.1rem] items-center rounded-lg hover:bg-[#5a1513] transition-colors delay-250 duration-[3000] ease-in" 
                 onClick={onClose}>
             Cancel
           </button>
