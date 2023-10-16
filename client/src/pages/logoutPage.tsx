33376c4799679813e617f576ed3752ad43f8966f
henneh <galvehannah5302@gmail.com>
2023-10-16 19:49:20 +0800

    responsiveness check

--- client/src/pages/logoutPage.tsx
+++ client/src/pages/logoutPage.tsx
@@ -12,21 +12,22 @@ const LogoutPage = () => {
 
   return (
     <div className='animate-fade-in relative'>
-      <div className='h-[10vh] w-full border-b-8 border-red-600'>
+      <div className='h-[10vh] w-full border-b-8 border-red-600 lg:border-b-[0.3rem]'>
         <img className='max-w-[9%] h-auto ml-[7vh] mt-[1.5vh] pt-[10px]' src={logo}/>
       </div>
       
       <div className='flex flex-row justify-even place-items-center bg-logout'>
         <div className='flex flex-col'>
-            <h1 className='font-extrabold text-9xl ml-20 text-red-700'>You have been logged out.</h1>
-            <span className='font-semibold text-4xl ml-20 mt-[50px]'>Thank you for using Reservo.<br/>To access the page, please log in again.</span>     
+            <h1 className='font-extrabold text-9xl ml-20 text-red-700 lg:text-7xl'>You have been logged out.</h1>
+            <span className='font-semibold text-4xl ml-20 mt-[50px] lg:text-2xl'>Thank you for using Reservo.<br/>To access the page, please log in again.</span>     
           <div className='flex'>
-            <Link to={'/uslogin'} className='bg-red-700 rounded-full text-center text-white text-3xl font-semibold py-3 px-6 mt-6 ml-20 m-[1rem] hover:bg-[#A01B00] transition-colors delay-250 duration-[3000] ease-in'>Return to Login Page</Link>
+            <Link to={'/uslogin'} className='bg-red-700 rounded-full text-center text-white text-3xl font-semibold py-3 px-6 mt-6 ml-20 m-[1rem] 
+            hover:bg-[#A01B00] transition-colors delay-250 duration-[3000] ease-in lg:text-xl'>Return to Login Page</Link>
           </div>
         </div>
 
         <div>
-          <img src={PeopleWaving} alt="people-waving" className='w-[900px] mr-[500px]'/>
+          <img src={PeopleWaving} alt="people-waving" className='w-[900px] mr-[500px] lg:w-[40rem]'/>
         </div>
       </div>
     </div>
