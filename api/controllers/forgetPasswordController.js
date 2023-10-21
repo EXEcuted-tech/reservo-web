6995ce200a2e142e0346b8b02048323788930681
Katteu <katheamari@gmail.com>
2023-10-22 02:17:17 +0800

    Forgot Password Improved

--- api/controllers/forgetPasswordController.js
+++ api/controllers/forgetPasswordController.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 // Define a POST route for sending reset emails
 
+const otpData = {};
+
 const sendEmail = async (req, res) => {
+console.log("Here");
   const { email } = req.body;
 
   // Create a Nodemailer transporter with your email service credentials
@@ -16,24 +19,56 @@ const sendEmail = async (req, res) => {
     },
   });
 
+  //Generate OTP Code
+  let string = '0123456789';
+  let OTP = '';
+
+  let len = string.length;
+  for (let i = 0; i < 6; i++) {
+      OTP += string[Math.floor(Math.random() * len)];
+  }
+
+  // Store OTP and creation timestamp
+  const timestamp = Date.now(); // Current timestamp
+  otpData[email] = { otp: OTP, timestamp };
+
   // Create the email message
   const mailOptions = {
     from: 'reservoph@gmail.com',
     to: email,
     subject: 'Password Reset Request',
-    text: 'Here is your password reset link...',
+    html: `<p>Your verification code is:</p><h2>${OTP}</h2><p>This code will expire in 5 minutes!</p><p>If you did not initiate this, you can safely ignore this message.</p>`,
   };
 
   try {
     // Send the email
     await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: 'Email sent successfully' });
+    res.status(200).json({ message: 'Email sent successfully'});
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Error sending email' });
   }
 }
 
+const validateOTP = (req, res) => {
+  const { email, userOTP } = req.body;
+
+  if (otpData[email]) {
+    const { otp, timestamp } = otpData[email];
+    const currentTime = Date.now();
+
+    // Check if the OTP is correct and within the time limit (e.g., 5 minutes)
+    if (userOTP === otp && currentTime - timestamp <= 5 * 60 * 1000) {
+      res.status(200).json({ message: 'OTP is valid', success:true});
+    } else {
+      res.status(200).json({ message: 'Invalid or expired OTP',success:false});
+    }
+  } else {
+    res.status(200).json({ message: 'OTP not found',success:false });
+  }
+};
+
 module.exports = {
-  sendEmail
+  sendEmail,
+  validateOTP
 }
