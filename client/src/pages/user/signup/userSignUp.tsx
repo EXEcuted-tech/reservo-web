d2d7eb8dc307dbb4452ddd47f5860fdae491eb9b
Lorenz Gil Suico <19101360@usc.edu.ph>
2023-10-27 21:32:18 +0800

    Temporary push. No merge request

--- client/src/pages/user/signup/userSignUp.tsx
+++ client/src/pages/user/signup/userSignUp.tsx
@@ -15,57 +15,55 @@ import { Spinner } from '@material-tailwind/react/components/Spinner';
 
 const SignupPage = () => {
   const navigate = useNavigate();
-  const [emailAdd, setEmailAdd]= useState("");
-  const [accName, setAccName]= useState("");
-  const [password, setPassword]= useState("");
-  const [contactNum, setContactNum]= useState("");
-  const [error,setError] = useState("");
+  const [emailAdd, setEmailAdd] = useState("");
+  const [accName, setAccName] = useState("");
+  const [password, setPassword] = useState("");
+  const [contactNum, setContactNum] = useState("");
+  const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const Navigate = useNavigate();
 
-  const signUp = (event:FormEvent) =>{
+  const signUp = (event: FormEvent) => {
     event.preventDefault();
     setIsLoading(true);
-    axios.post(`${config.API}/signup`,{
-        account_name:  accName, 
-        account_email: emailAdd,
-        password : password,
-        account_type: 1,
-        contact_number: contactNum
-    }).then((res)=>{
-        if (res.data.success == true){
-          setTimeout(()=>{
-            setIsLoading(false);
-          },1500);
-          alert("Registered Successfully!");
-          Navigate('/uslogin');
-        }else{
-          setTimeout(()=>{setIsLoading(false)},800);
-          setError(res.data.error);
-        }
-    }).catch((err) => { 
-      console.log("ERROR: ",err);
-        setError(err.error);
-        setIsLoading(false);
+    axios.post(`${config.API}/signup`, {
+      account_name: accName,
+      account_email: emailAdd,
+      password: password,
+      account_type: 1,
+      contact_number: contactNum
+    }).then((res) => {
+      if (res.data.success == true) {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1500);
+        alert("Registered Successfully!");
+        Navigate('/uslogin');
+      } else {
+        setTimeout(() => { setIsLoading(false) }, 800);
+        setError(res.data.error);
+      }
+    }).catch((err) => {
+      console.log("ERROR: ", err);
+      setError(err.error);
+      setIsLoading(false);
     });
   }
 
   return (
     <div className='animate-fade-in content-center overflow-hidden font-poppins'>
-      {error !='' && <Danger message={error}/> }
+      {error != '' && <Danger message={error} />}
       {/* Background Picture */}
       <img className='absolute h-screen w-full' src={background} />
-      <div className='absolute left-2/4 top-2/4 text-align w-[1000px] p-[10px] -translate-x-2/4 -translate-y-2/4 xl:max-2xl:scale-[0.67] sm:max-xl:w-[90%]'>
+      <div className='absolute left-2/4 top-2/4 text-align w-[1000px] p-[10px] -translate-x-2/4 -translate-y-2/4 xl:max-2xl:scale-[0.67]'>
         {/* Left Box */}
-        <div className='w-[35%] bg-[#DD2803] float-left text-center shadow-[12px_23px_10px_-4px_gray] pt-[70px] pb-[78px] px-10 rounded-[7px_0px_0px_7px] flex flex-col justify-center sm:max-xl:h-[45vh] sm:max-xl:w-full'>
-
-        {/* <div className="  w-full xl:w-1/3 h-[63vh]"></div> */}
-          <img className='max-w-[40%] h-auto ml-[32%] sm:max-xl:h-[70px] sm:max-xl:w-[70px]' src={guykey} />
+        <div className='w-[35%] bg-[#DD2803] float-left text-center shadow-[12px_23px_10px_-4px_gray] pt-[70px] pb-[78px] px-10 rounded-[7px_0px_0px_7px]'>
+          <img className='max-w-[40%] h-auto ml-[32%]' src={guykey} />
           {/* Welcome Sign */}
-          <div className='m-0.5 p-0 border-y-2 border-y-[white] border-solid  font-bold font-poppins'>
-            <h2 className='block text-[1.7em] m-[5.45%] text-[white]'>Sign Up to<br/> Reservo!</h2>
+          <div className='m-2.5 p-0 border-y-2 border-y-[white] border-solid  font-bold font-poppins'>
+            <h2 className='block text-[1.7em] m-[5.45%] text-[white]'>Sign Up to<br /> Reservo!</h2>
           </div>
-          <p className='text-white'>Access more features by <br/>signing up now!</p>
+          <p className='text-white'>Access more features by <br />signing up now!</p>
           <div className='mt-[53px] font-bold font-poppins'>
             <h3 className=' mb-0 text-[1.17em] text-white'>Already have an</h3>
             <h3 className=' mb-2 text-[1.17em] text-white'>existing account?</h3>
@@ -73,53 +71,52 @@ const SignupPage = () => {
             <a className='no-underline inline-block text-[#e72a2a] border text-lg relative cursor-pointer font-[bold] 
                           w-[190px] px-6 py-[11px] bg-white
                           rounded-[100px] font-poppins font-bold hover:bg-[#9a1a00] hover:border-[#9a1a00] hover:text-white transition-colors delay-250 duration-[3000] ease-in'
-                          onClick={()=>{navigate('/uslogin')}}>
+              onClick={() => { navigate('/uslogin') }}>
               Log In
             </a>
           </div>
         </div>
         {/* Right Box */}
-        <div className='w-[65%] float-left bg-[white] shadow-[4px_15px_10px_4px_gray] pt-[38px] pb-[50px] px-[100px] rounded-[0px_7px_7px_0px] sm:max-xl:h-[50vh] sm:max-xl:w-full'>
-
+        <div className='w-[65%] float-left bg-[white] shadow-[4px_15px_10px_4px_gray] pt-[38px] pb-[50px] px-[100px] rounded-[0px_7px_7px_0px]'>
           <h2 className='text-center text-[25px] font-bold font-poppins'>Create Your Account</h2>
           <div className='mt-2.5'>
-            <BsFillPersonFill className='float-left text-[21px]'  />
+            <BsFillPersonFill className='float-left text-[21px]' />
             <label className='float-left ml-[2px]'>Username</label>
             <input type="username" className='w-full inline-block border rounded box-border bg-[#EDF5F3] mx-0 my-2 px-5 py-3 border-solid border-[#ccc]'
-                value={accName}
-                onChange={(e) => setAccName(e.target.value)}            
+              value={accName}
+              onChange={(e) => setAccName(e.target.value)}
             ></input>
           </div>
           <div className='mt-2.5'>
-            <MdEmail className='float-left text-[20px]'/>
-            <label  className='float-left ml-[3px]'>Email</label>
+            <MdEmail className='float-left text-[20px]' />
+            <label className='float-left ml-[3px]'>Email</label>
             <input type="username" className='w-full inline-block border rounded box-border bg-[#EDF5F3] mx-0 my-2 px-5 py-3 border-solid border-[#ccc]'
-            value={emailAdd}
-            onChange={(e) => setEmailAdd(e.target.value)}></input>
+              value={emailAdd}
+              onChange={(e) => setEmailAdd(e.target.value)}></input>
           </div>
           {/* //Halu */}
           <div className='mt-2.5'>
-            <BsFillTelephoneFill className='float-left text-[17px]'/>
+            <BsFillTelephoneFill className='float-left text-[17px]' />
             <label className='float-left ml-[4px]'>Contact Number</label>
             <input type="username" className='w-full inline-block border rounded box-border bg-[#EDF5F3] mx-0 my-2 px-5 py-3 border-solid border-[#ccc]'
-            value={contactNum}
-            onChange={(e) => setContactNum(e.target.value)}
+              value={contactNum}
+              onChange={(e) => setContactNum(e.target.value)}
             ></input>
           </div>
           <div className='mt-2.5'>
-            <BiSolidLockAlt className='float-left text-[19px]'/>
+            <BiSolidLockAlt className='float-left text-[19px]' />
             <label className='float-left ml-[3px]'>Password</label>
             <input type="password" className='w-full inline-block border rounded box-border bg-[#EDF5F3] mx-0 my-2 px-5 py-3 border-solid border-[#ccc]'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             ></input>
           </div>
           <div className='flex justify-center mt-5'>
             <button type='submit' onClick={signUp} className='flex items-center justify-center no-underline inline-block text-[white] border text-lg relative cursor-pointer
             shadow-[inset_0_0_0_white] w-[220px] px-6 py-[11px] rounded-[100px] border-solid border-[#e72a2a] font-poppins font-bold bg-[#DD2803] hover:bg-[#9a1a00] hover:border-[#9a1a00] hover:text-white transition-colors delay-250 duration-[3000] ease-in'>
-                    {isLoading && <Spinner className='mr-[1%]'/>}
-                    Sign Up                                                 
-              </button> 
+              {isLoading && <Spinner className='mr-[1%]' />}
+              Sign Up
+            </button>
           </div>
 
         </div>
