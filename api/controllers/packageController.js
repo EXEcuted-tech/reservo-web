c2075da92d75920b1b1427284c01b6ae5cfda25f
Angelou Sereño <angeloubendebelsereno@gmail.com>
2023-11-18 00:40:36 +0800

    added deadline checker and automatic un/publish

--- api/controllers/packageController.js
+++ api/controllers/packageController.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const db = require('./a_db'); 
+const { forEach } = require('../validations/packageValidator');
 
 const createPackage = (req,res)=>{
+  refreshDeadline()
 
     const {
       package_name,
@@ -40,7 +42,9 @@ const createPackage = (req,res)=>{
 }
 
 const updatePackage = (req, res) => {
+  refreshDeadline
   const inputData = req.body; // Use the JSON object directly
+  console.log("INPUT BEH==>", inputData);
 
   // Construct the SET clause for the SQL update statement
   const setClauses = Object.keys(inputData)
@@ -77,8 +81,96 @@ const updatePackage = (req, res) => {
   });
 };
 
+const localUpdatePackage = (req) => {
+  const inputData = req.body; // Use the JSON object directly
+  console.log("INPUT BEH==>", inputData);
+
+  // Construct the SET clause for the SQL update statement
+  const setClauses = Object.keys(inputData)
+    .filter(key => key !== 'package_id') // Exclude package_id from the update
+    .map(key => `${key} = ?`)
+    .join(', ');
+
+  // Create the data array by mapping values from the JSON object
+  const data = Object.keys(inputData)
+    .filter(key => key !== 'package_id') // Exclude package_id from the data
+    .map(key => inputData[key]);
+
+  // Add the package_id to the end of the data array
+  data.push(inputData.package_id);
+
+  const update = `UPDATE package SET ${setClauses} WHERE package_id = ?`;
+
+  db.query(update, data, (err, result) => {
+    if (err) {
+      console.error('Error updating data:', err);
+      return JSON.stringify({ status: 500, success: false, error: 'Error updating data' });
+    }
+
+    if (result.affectedRows > 0) {
+      console.log(result)
+      return JSON.stringify({
+        status: 200,
+        success: true,
+        data: result,
+      });
+    } else {
+      return JSON.stringify({ status: 500, success: false, error: 'Record update failed' });
+    }
+  });
+};
+
+const refreshDeadline = () => {
+  db.query('SELECT * FROM package', (err, rows) => {
+    if (err) {
+      return res.status(500).json({ message: "Failed to get data!" })
+    } else {
+      rows.forEach(element => {
+        var dateNow = new Date();
+
+        // Check date_end and time_end for setting visibility to NOT PUBLISHED
+        if (element.date_end && dateNow >= element.date_end) {
+          if (element.visibility === "PUBLISHED") {
+            var body = { ...element, visibility: "NOT PUBLISHED" };
+            var req = { body };
+            localUpdatePackage(req);
+            console.log("Updated Package: ", element.package_name, " to NOT PUBLISHED");
+          }
+        } else {
+          // Check time_end for setting visibility to NOT PUBLISHED
+          if (element.time_end) {
+            var timeNow = new Date().toLocaleTimeString('en-US', { hour12: false });
+            if (timeNow > element.time_end && element.visibility === "PUBLISHED") {
+              var body = { ...element, visibility: "NOT PUBLISHED" };
+              var req = { body };
+              localUpdatePackage(req);
+              console.log("Updated Package: ", element.package_name, " to NOT PUBLISHED");
+            } else {
+              console.log("Package", element.package_name, "is Active");
+            }
+          } else {
+            console.log("Package", element.package_name, "is Active");
+          }
+
+          // Check date_start and time_start for setting visibility to PUBLISHED
+          if (element.date_start && dateNow >= element.date_start && element.time_start) {
+            var timeNow = new Date().toLocaleTimeString('en-US', { hour12: false });
+            if (timeNow >= element.time_start && element.visibility === "NOT PUBLISHED") {
+              var body = { ...element, visibility: "PUBLISHED" };
+              var req = { body };
+              localUpdatePackage(req);
+              console.log("Updated Package: ", element.package_name, " to PUBLISHED");
+            }
+          }
+        }
+      });
+    }
+  });
+}
+
 
 const retrieveAll = (req,res)=>{
+  refreshDeadline()
     const retrieveRecs = 'SELECT * FROM package'
 
     db.query(retrieveRecs, (err, rows) => {
@@ -96,6 +188,7 @@ const retrieveAll = (req,res)=>{
 }
 
 const retrieveByParams = (req,res)=>{
+  refreshDeadline()
     const { col, val } = req.query; 
 
     const retrieveSpecific = 'SELECT * FROM package WHERE ?? = ?';
@@ -115,6 +208,7 @@ const retrieveByParams = (req,res)=>{
 }
 
 const retrieveByTwoParams = (req,res)=>{
+  refreshDeadline()
   const { col1, val1, col2, val2, order_param} = req.query; 
   var orderByClause = ''; // Initialize the ORDER BY clause
 
@@ -141,6 +235,7 @@ const retrieveByTwoParams = (req,res)=>{
 }
 
 const deletePackage = (req,res)=>{
+  refreshDeadline()
 
     const {package_id} = req.body;
 
@@ -161,6 +256,7 @@ const deletePackage = (req,res)=>{
 }
 
 const retrieveMinMaxPrices = (req, res) => {
+  refreshDeadline()
   const {merch_id} = req.query;
 
   const retrieveMinPriceQuery = `SELECT MIN(price) AS min_price FROM package WHERE merchant_id = ?`;
@@ -198,4 +294,5 @@ module.exports = {
     deletePackage,
     retrieveByTwoParams,
     retrieveMinMaxPrices,
+    refreshDeadline,
 }
