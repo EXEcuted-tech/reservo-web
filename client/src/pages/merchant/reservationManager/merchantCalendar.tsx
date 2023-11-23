93c196e07f95917189f54115173634b411e3fb1a
lourdsam <17100411@usc.edu.ph>
2023-11-23 20:26:04 +0800

    set sched

--- client/src/pages/merchant/reservationManager/merchantCalendar.tsx
+++ client/src/pages/merchant/reservationManager/merchantCalendar.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react'
-import { Fragment, useRef, useState } from "react";
+import { useEffect, useState } from 'react'
+import { Fragment, useRef } from "react";
 import MerchAdHeader from '../../../components/headers/MerchAdHeader'
 import { RiReservedFill } from 'react-icons/ri'
 import MerchantLayout from '../../../components/layout/MerchantLayout';
 import ButtonC from '../../../components/calendar/ButtonC';
 import { GrNext, GrPrevious } from 'react-icons/gr';
 import Button from './Component/Button';
+import Calendar from 'react-calendar';
 
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const CalendarMerchant = () => {
 
@@ -28,6 +32,8 @@ const CalendarMerchant = () => {
   const [dEnd, setEnd] = useState(getDaysInMonth(year, ndx))
   const [dStart, setStart] = useState(getFirstDayOfMonth(year, ndx))
 
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [valuedate, onChange] = useState<Value>(new Date());
 
   useEffect(() => {
     setEnd(getDaysInMonth(year, ndx));
@@ -38,8 +44,68 @@ const CalendarMerchant = () => {
   console.log("year: " + year + " monthNDX: " + ndx);
   console.log("startDate: " + dStart + "endDate:" + dEnd)
 
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  const [selectedDate, setSelectedDate] = useState<Value>(new Date());
+  const [selectedTime, setSelectedTime] = useState<string>(''); // Add state for time
+
+  const handleDateChange = (date: Date | null) => {
+    if (date) {
+      setSelectedDate(date);
+    }
+  };
+
+  const handleTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedTime(e.target.value);
+  };
+
   return (
     <>
+    <div className='flex justify-end mb-2'>
+        <button onClick={handleOpenModal} className='bg-red-700 text-white py-2 px-4 rounded'>Set Schedule</button>
+        {isModalOpen && (
+          <div>
+          <div className='fixed top-0 left-0 w-full h-full bg-black opacity-50'></div>
+          <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center'>
+            <div className='bg-white p-8 rounded shadow-lg'>
+              <div className='my-4'>
+                <p className='text-2xl font-bold'>Select date range for unavailability.</p>
+                <div className='p-1'>
+                  <label>Date Start:</label>
+                  <input type="date" onChange={(e) => handleDateChange(new Date(e.target.value))} />
+                </div>
+                <div className='p-1'>
+                  <label>Date End:</label>
+                  <input type="date" onChange={(e) => handleDateChange(new Date(e.target.value))} />
+                </div>
+              </div>
+              <div className='mb-2'>
+                <p className='text-2xl font-bold'>Select time range for unavailability.</p>
+                <div className='p-1'>
+                  <label>Time Start:</label>
+                  <input type="time" value={selectedTime} onChange={handleTimeChange} />
+                </div>
+                <div className='p-1'>
+                  <label>Time End:</label>
+                  <input type="time" value={selectedTime} onChange={handleTimeChange} />
+                </div>
+              </div>
+              <div className='flex justify-end col-span-2'>
+                <button className='mt-4 mr-2 bg-blue-500 text-white py-2 px-4 rounded'>Save</button>
+                <button onClick={handleCloseModal} className='mt-4 bg-red-500 text-white py-2 px-4 rounded'>Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        )}
+      </div>
       <div className="calMerch grid w-full">
         <div className='flex flex-cols justify-center items-center bg-sky-500'>
           <button onClick={(e) => {
