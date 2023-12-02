9b646f14d6b1c9c83de1199bc1c1dedac10112a5
Angelou Sereño <angeloubendebelsereno@gmail.com>
2023-12-02 19:06:59 +0800

    cors changes

--- api/routes.js
+++ api/routes.js
@@ -16,28 +16,29 @@ const feedbackRoutes = require('./routes/feedbackRoutes')
 const forgetPasswordRoutes = require('./routes/forgetPasswordRoutes')
 const fileRoutes = require('./routes/fileRoutes')
 
+const allowedOrigins = process.env.CORS_ORIGIN.split(',');
+
 app.use(cors({
-    origin: [process.env.CORS_ORIGIN],
-    methods: ["GET","POST"],
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
     credentials: true,
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+    res.header('Access-Control-Allow-Origin', allowedOrigins.join(','));
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
-const allowedOrigins = process.env.CORS_ORIGIN.split(',');
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
 }));
 
 app.use(express.json());
